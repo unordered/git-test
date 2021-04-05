9fbe79b9dc5b64f2b02f752023fd28c2234e5399
unordered@outlook.kr <unordered@outlook.kr>
2021-04-05 09:05:15 +0900

    초기 커밋.

---
+++ git-test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_test
+{
+    class Program
+    {
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            //git test 1
+            int sum = 0;
+            for(int i=0; i<10; i++)
+            {
+                sum += Sum(i, i + 1);
+            }
+
+
+        }
+    }
+}
